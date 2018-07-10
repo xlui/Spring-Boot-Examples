ade3bdfa8e8a1512620cd38085268e59933b9e24
xlui <liuqi0315@gmail.com>
2018-07-10 16:47:24 +0000

    Spring Boot Integrate with Kafka

---
+++ spring-boot-kafka/consumer/src/main/java/me/xlui/example/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package me.xlui.example.kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+	@KafkaListener(topics = {"kafka"})
+	public void receiveMessage(String message) {
+		System.out.println("Receive: [" + message + "]");
+	}
+}
---
+++ spring-boot-kafka/producer/src/main/java/me/xlui/example/kafka/producer/ProducerApplication.java
@@ -0,0 +1,26 @@
+package me.xlui.example.kafka.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class ProducerApplication implements CommandLineRunner {
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		kafkaTemplate.send("kafka", "hello kafka!");
+		kafkaTemplate.send("kafka", "current time: " + new Date());
+		System.out.println("Successfully send");
+	}
+}
